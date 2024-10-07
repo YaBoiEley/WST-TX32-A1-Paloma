cccd0c87bdde30d588a61d6048fdab0cb5db5697
YaBoiEley <axlisatschool@gmail.com>
2024-10-07 23:00:23 +0800

    Feat - fragmented the header and footer in home, info, and about tab

--- app/Views/aboutPage.php
+++ app/Views/aboutPage.php
@@ -16,30 +16,8 @@
         <?= $this ->include('Components/header_nav') ?>
 
         <!-- Footer -->
-        <div class="darkblue pb-3">
-            <div class = "container text-center text-md-left mb-5">
-                <div class ="row card-footer border-white border-opacity-10">
-                    <div class="mx-auto pt-5 col-md-4 col-lg-4 col-xl-4">
-                        <h3 class = "fw-bold cyan">GROUP 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada efficitur dignissim. Fusce et leo at nisl condimentum pellentesque vel blandit odio. Suspendisse potenti. Vestibulum dui nunc, molestie a ex nec, rutrum euismod eros. Nulla lacinia eros nec tincidunt tincidunt. Cras a auctor mauris. Nunc sed nibh accumsan, ullamcorper est sed, consectetur mauris.</p>
-                    </div>
+        
+        <?= $this ->include('Components/footer_main') ?>
 
-                    <div class="mx-auto pt-5 col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                        <h3 class = "fw-bold cyan">GROUP 1</h3>
-                        <p class = "center">N/A</p>
-                        <p class = "center">N/A</p>
-                        <p class = "center">N/A</p>
-                        <p class = "center">N/A</p>
-                    </div>
-
-                    <div class="mx-auto pt-5 col-8 col-sm-9 col-md-3 col-lg-3 col-xl-3">
-                        <h3 class = "fw-bold cyan">Contacts</h3>
-                        <p><i class="fa fa-home me-3" aria-hidden="true"></i>P. Paredes St, Sampaloc, Manila, 1015 Metro Manila</p>
-                        <p><i class="fa fa-envelope me-3" aria-hidden="true"></i>group1@email.com</p>
-                        <p><i class="fa fa-phone me-3" aria-hidden="true"></i>63+ 900-000-0000</p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </body>
 </html>
