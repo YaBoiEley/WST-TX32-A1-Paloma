16c4be913063c843925ef4c5935ebe2aba8d633f
YaBoiEley <axlisatschool@gmail.com>
2024-10-07 22:38:23 +0800

    Feat - fragmented the header, added a snippet

--- app/Views/landingPage.php
+++ app/Views/landingPage.php
@@ -12,35 +12,8 @@
 
     <body>
         <!-- Header -->
-        <nav class="navbar navbar-expand-sm navbar-dark navbar-custom">
-            <div class="container">
-                <a href="<?= base_url() ?>" class="navbar-brand text-uppercase fs-5">
-                    <img src="" alt="Logo" width="70" class="d-inline-block align-middle">
-                    GROUP 1 TEMPLATE
-                </a>
-
-                <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="list1">
-                    <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a href="<?= base_url() ?>" class="nav-link">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="<?= base_url() . 'aboutUs' ?>" class="nav-link">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="<?= base_url() . 'travelInformation' ?>" class="nav-link">Info</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="groupPage.php" class="nav-link">Group</a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        
+            <?= $this ->include('Components/header_nav') ?>
 
         <!-- Footer -->
         <div class="darkblue pb-3">
